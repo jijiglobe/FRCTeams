065650ed57b753f7d8445b8f8f11684ea3134d31
elc1798 <subliminalmau5@gmail.com>
2015-11-15 18:12:17 -0500

    Convert documentation to standard python docs

--- query.py
+++ query.py
@@ -3,17 +3,19 @@ domain = "www.thebluealliance.com"
 api = "/api/v2/%s"
 hdr = {"X-TBA-App-Id": "frc694:testing-app:v01"}
 
-#Gets teamNumber's team information formatted as follows:
-#    {"website": Team's official website URL,
-#    "name": longform name of team",
-#    "nickname": Teams nickname or functional name,
-#    "locality": Team's city,
-#    "region": Team's state or region,
-#    "team_number": Team's number,
-#    "location": Longform locality address,
-#    "country_name": Name of team's country,
-#    "rookie_year": Team's rookie year"}
 def getTeamData(teamNumber):
+    """
+    Gets teamNumber's team information formatted as follows:
+        "website": Team's official website URL,
+        "name": longform name of team",
+        "nickname": Teams nickname or functional name,
+        "locality": Team's city,
+        "region": Team's state or region,
+        "team_number": Team's number,
+        "location": Longform locality address,
+        "country_name": Name of team's country,
+        "rookie_year": Team's rookie year"
+    """
     conn = httplib.HTTPConnection(domain)
     path = "team/frc"+str(teamNumber)
     conn.request("GET",api % path ,headers = hdr)
@@ -21,28 +23,33 @@ def getTeamData(teamNumber):
     answer = r.read().decode('utf-8')
     return json.loads(answer)
 
-#helper function to take an  array of matches (as retrieved in getAllMatches()) and converts to URL strings
 def getVideoList(array):
+    """
+    Function to take a list of matches (as retrieved in getAllMatches())
+    and converts them to URL strings
+    """
     ans = []
     for x in array:
         if x["type"] == "youtube":
             ans += "www.youtube.com/watch?v="+x["key"]
     return ans
 
-#Returns the requested event data from the requested year as follows:
-#    {"name": name of event,
-#    "short_name": short name of event,
-#    "event_type-string": string defining event type,
-#    "event_district-string": string defining disctrict of event,
-#    "location": Longform address of city,
-#    "venue_address": Longform address of the venue,
-#    "website": event website string,
-#    "teams": dictionary of teams at event(formatted as above in getTeamData),
-#    "matches": dictionary of match data(formatted as below in getMatchData),
-#    "alliances": Json array of alliances:
-#                 "picks": [captain,team2,team3]
-#                 "declines": [team1,team2, team3]
 def getEventData(year, code):
+    """
+    Returns the requested event data from the requested year as follows:
+        "name": name of event,
+        "short_name": short name of event,
+        "event_type-string": string defining event type,
+        "event_district-string": string defining disctrict of event,
+        "location": Longform address of city,
+        "venue_address": Longform address of the venue,
+        "website": event website string,
+        "teams": dictionary of teams at event(formatted as above in getTeamData),
+        "matches": dictionary of match data(formatted as below in getMatchData),
+        "alliances": Json array of alliances:
+            "picks": [captain,team2,team3]
+            "declines": [team1,team2, team3]
+    """
     conn = httplib.HTTPConnection(domain)
     path = "event/"+str(year)+code
     conn.request("GET",api % path ,headers = hdr)
@@ -51,22 +58,24 @@ def getEventData(year, code):
     final = json.loads(answer)
     return final
     
-#returns list of matches formatted as:
-#    {"key": match key,
-#    "comp_level": level of competition ie qm,em,qf,sf,f,
-#    "alliances": {
-#        "blue": {
-#            "score": match score
-#            "teams": list of team keys ie [frc1, frc694, frc9999]
-#        }
-#        "red": {
-#            "score": match score
-#            "teams": list of team keys ie [frc1, frc694, frc9999]
-#        }
-#    "videos" JSON array of available match videos
-#    "time_string": string of time scheduled for match,
-#    "time": Unix time scheduled for match}
 def getAllMatches(year, code):
+    """
+    Returns list of matches formatted as:
+        "key": match key,
+        "comp_level": level of competition ie qm,em,qf,sf,f,
+        "alliances": {
+            "blue": {
+                "score": match score
+                "teams": list of team keys ie [frc1, frc694, frc9999]
+            }
+            "red": {
+                "score": match score
+                "teams": list of team keys ie [frc1, frc694, frc9999]
+            }
+        "videos" JSON array of available match videos
+        "time_string": string of time scheduled for match,
+        "time": Unix time scheduled for match}
+    """
     conn = httplib.HTTPConnection(domain)
     path = "event/"+str(year)+code+"/matches"
     conn.request("GET",api % path ,headers = hdr)
@@ -77,28 +86,33 @@ def getAllMatches(year, code):
         x["videos"] = getVideoList(x["videos"])
     return final
 
-#takes an element of getAllMatches List and returns the match scores as follows:
-#    {"blue": score for blue,
-#    "red: score for red}
 def getMatchScore(match):
-    return {"blue": match["score_breakdown"]["blue"]["total_points"],
-            "red": match["score_breakdown"]["red"]["total_points"]
+    """
+    Takes an element of getAllMatches List and returns the match scores as follows:
+        "blue": score for blue,
+        "red: score for red}
+    """
+    return {
+        "blue": match["score_breakdown"]["blue"]["total_points"],
+        "red": match["score_breakdown"]["red"]["total_points"]
     }
 
-#returns list of events. each event is a dict as follows:
-#    {"code": event code(can be used to pull event data in other functions),
-#    "short_name": short name of event,
-#    "event_type-string": string defining event type,
-#    "event_district-string": string defining disctrict of event,
-#    "location": Longform address of city,
-#    "venue_address": Longform address of the venue,
-#    "website": event website string,
-#    "teams": dictionary of teams at event(formatted as above in getTeamData),
-#    "matches": dictionary of match data(formatted as below in getMatchData),
-#    "alliances": Json array of alliances:
-#                 "picks": [captain,team2,team3]
-#                 "declines": [team1,team2, team3]
 def getEventList(year):
+    """
+    Returns list of events. each event is a dict as follows:
+        "code": event code(can be used to pull event data in other functions),
+        "short_name": short name of event,
+        "event_type-string": string defining event type,
+        "event_district-string": string defining disctrict of event,
+        "location": Longform address of city,
+        "venue_address": Longform address of the venue,
+        "website": event website string,
+        "teams": dictionary of teams at event(formatted as above in getTeamData),
+        "matches": dictionary of match data(formatted as below in getMatchData),
+        "alliances": Json array of alliances:
+            "picks": [captain,team2,team3]
+            "declines": [team1,team2, team3]
+    """
     conn = httplib.HTTPConnection(domain)
     path = "events/"+str(year)
     conn.request("GET",api % path ,headers = hdr)
@@ -106,4 +120,4 @@ def getEventList(year):
     answer = r.read().decode('utf-8')
     final = json.loads(answer)
     return final
-    
+ 
